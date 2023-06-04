22ad531032bb576b2ddcfd61208828ee2aa77a25
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-06-04 16:58:58 -0300

    feat: add lib dotenv and created the archive

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { sendWhatsappMessage } from './services/twilio';
+import dotenv from 'dotenv';
 
 const app = express();
 const port = 3333;
@@ -9,6 +10,8 @@ const port = 3333;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+
+dotenv.config();
 
 app.post('chat/send', async (req, res) => {
   const { to, body } = req.body;
--- src/services/twilio.ts
+++ src/services/twilio.ts
@@ -1,8 +1,8 @@
 import twilio from "twilio";
 
-const accountSid = ""
-const authToken = ""
-const whatsappPhoneNumber = ""
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const whatsappPhoneNumber = process.env.WHATSAPP_PHONE_NUMBER;
 
 const client = twilio(accountSid, authToken);
 
