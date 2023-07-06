7a5b657e5098c4a5a561937277249d5ad61ce82e
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-07-06 20:42:26 -0300

    feat: Complete openai settings

--- src/services/openai.ts
+++ src/services/openai.ts
@@ -5,4 +5,19 @@ dotenv.config();
 
 const config = new Configuration({
   apiKey: process.env.OPENAIA_API_KEY
-})
+});
+
+const openai = new OpenAIApi(config);
+
+export const getOpenAICompletion = async (input: string): Promise<string> => {
+  try{
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: input}]
+    })
+    return completion.data.choices[0].message?.content as string;
+  }catch (error) {
+    console.log(`Error completing input: ${error}`);
+    return '';
+  }
+}
